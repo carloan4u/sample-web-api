f385b686adf17125c85a297cbd4f55438e8ceb70
srinivas.sunkari <srinivas.sunkari@zuto.com>
2017-06-27 14:08:34 +0100

    added the terraform scripts to the application

---
+++ terraform/eu-west-2-prod/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "zuto-terraform-state-files"
+    key    = "services/sample-web-api-prod.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "beanstalk-web-app" {
+  source = "github.com/carloan4u/terraform-aws-beanstalk-environment-module?ref=v1.0.1"
+  app_name = "sample-web-api"
+  app_environment = "prod"
+  asg_min_instances = 1
+  asg_max_instances = 3
+}
---
+++ terraform/eu-west-2-qa/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket = "zuto-terraform-state-files"
+    key    = "services/sample-web-api-qa.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_elastic_beanstalk_application" "default" {
+  name = "sample-web-api"
+  description = "Sample Application"
+}
+
+module "beanstalk-web-app" {
+  source = "../../../terraform-aws-beanstalk-environment-module"
+  app_name = "${aws_elastic_beanstalk_application.default.name}"
+  app_environment = "qa"
+  asg_min_instances = 1
+  asg_max_instances = 2
+}
