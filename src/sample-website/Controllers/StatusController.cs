022bf70e0bed58c347652f56952fc57570ab3433
Karl Atkinson <karl.atkinson@gmail.com>
2017-06-30 12:09:10 +0100

    added status controller

---
+++ src/sample-website/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+
+namespace sample_website.Controllers
+{
+    public class StatusController : ApiController
+    {
+        public dynamic Get()
+        {
+            return new
+            {
+                Status = ConfigurationManager.AppSettings["Environment"]
+            };
+        }
+    }
+}
