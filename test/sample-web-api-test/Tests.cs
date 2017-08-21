374d7930805649f9d8fc01dc584e0bc096381d2b
Dan Fox <dan.fox@zuto.com>
2017-08-21 13:09:28 +0100

    Added a unit test

---
+++ test/sample-web-api-test/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace sample_web_api_test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void The_Things_Should_Be_Equal()
+        {
+            Assert.That("This thing".Equals("the other thing"));
+        }
+    }
+}
