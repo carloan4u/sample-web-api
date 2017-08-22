310ef47285fd86fde802a6de71fd2a328cb73963
Karl Atkinson <karl.atkinson@gmail.com>
2017-08-22 15:06:26 +0100

    make app name a variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,26 @@
 provider "aws" {
-  region = "eu-west-2"
+  region  = "eu-west-2"
   profile = "zuto-dev"
 }
 
 resource "aws_elastic_beanstalk_application" "my_beanstalk_app" {
-  name        = "aws-workshop-terraform"
+  name        = "${var.app_name}"
   description = "my super cool beanstalk app"
 }
 
 module "beanstalk-web-app" {
-  source = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
-  app_name = "${aws_elastic_beanstalk_application.my_beanstalk_app.name}"
-  app_environment = "${var.app_environment}"
+  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.6"
+  app_name          = "${aws_elastic_beanstalk_application.my_beanstalk_app.name}"
+  app_environment   = "${var.app_environment}"
   asg_min_instances = 1
   asg_max_instances = 3
-  ec2_key = "dev-ec2-applications"
-  owner_tag = "${var.owner_tag}"
-  healthcheck_url = "/api/status"
+  ec2_key           = "dev-ec2-applications"
+  owner_tag         = "${var.owner_tag}"
+  healthcheck_url   = "/api/status"
 
   sns_topic = {
-   name = "${aws_elastic_beanstalk_application.my_beanstalk_app.name}-${var.app_environment}"
-   endpoint = "arn:aws:sqs:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_elastic_beanstalk_application.my_beanstalk_app.name}-${var.app_environment}"
-   protocol = "sqs"
- }
+    name     = "${aws_elastic_beanstalk_application.my_beanstalk_app.name}-${var.app_environment}"
+    endpoint = "arn:aws:sqs:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_elastic_beanstalk_application.my_beanstalk_app.name}-${var.app_environment}"
+    protocol = "sqs"
+  }
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,5 +1,7 @@
 data "aws_caller_identity" "current" {}
 
+variable "app_name" {}
+
 variable "app_environment" {
   default = "qa"
 }
