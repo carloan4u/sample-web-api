e3a8f92132d72671c5c79674797bfb78e8082731
Simon Kirkham <simon.kirkham@zuto.com>
2017-08-22 14:24:17 +0100

    Updating Terraform module version

--- deploy/terraform/eu-west-2-qa/main.tf
+++ deploy/terraform/eu-west-2-qa/main.tf
@@ -22,7 +22,7 @@ resource "aws_elastic_beanstalk_application" "default" {
 }
 
 module "beanstalk-web-app" {
-  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.4"
+  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
   app_name          = "${aws_elastic_beanstalk_application.default.name}"
   instance_type     = "t2.small"
   app_environment   = "${var.environment}"
