4652475055456c08b17af006407132ea8c07299d
Liam Reilly <liam.reilly@zuto.com>
2017-08-30 15:14:23 +0100

    Secure

--- src/sample-web-api/Controllers/StatusController.cs
+++ src/sample-web-api/Controllers/StatusController.cs
@@ -11,7 +11,8 @@ namespace sample_website.Controllers
             return new
             {
                 Status = HttpStatusCode.OK,
-                LiamsVariable = ConfigurationManager.AppSettings["LiamsVariable"]
+                LiamsVariable = ConfigurationManager.AppSettings["LiamsVariable"],
+                LiamsOtherVariable = ConfigurationManager.AppSettings["LiamsOtherVariable"],
             };
         }
     }
