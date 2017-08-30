c5f35a6905c6320d7fc335305b05341fd57427f3
Roderick Calder <Roderick.Calder@zuto.com>
2017-08-30 13:29:23 +0100

    terraform file update (qa)

--- deploy/terraform/eu-west-2-qa/main.tf
+++ deploy/terraform/eu-west-2-qa/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "zuto-terraform-state-files"
-    key    = "services/routing-service-qa.tfstate"
+    key    = "services/rods-application/qa.tfstate"
     region = "eu-west-2"
     acl    = "bucket-owner-full-control"
   }
@@ -18,8 +18,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_elastic_beanstalk_application" "default" {
-  name        = "routing-service"
-  description = "Loan application routing service"
+  name        = "rods-application"
+  description = "This is rods cool funky app"
 }
 
 module "beanstalk-web-app" {
@@ -27,11 +27,12 @@ module "beanstalk-web-app" {
   app_name          = "${aws_elastic_beanstalk_application.default.name}"
   instance_type     = "t2.small"
   app_environment   = "${var.environment}"
-  asg_min_instances = 2
+  asg_min_instances = 1
   asg_max_instances = 2
   ec2_key           = "qa-ec2-applications"
   create_dns_record = true
-  owner_tag         = "Sales-Ops"
+  owner_tag         = "R-Calder"
+  healthcheck_url   = "/api/status"
 
   sns_topic = {
     name = "${aws_elastic_beanstalk_application.default.name}-${var.environment}"
