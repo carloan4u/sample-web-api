13624365a209ae3618be9c6cf8dc6a8fab7413ff
andy.valentine <andy.valentine@zuto.com>
2017-09-06 16:24:17 +0100

    added sns configuration

--- deploy/terraform/eu-west-2-qa/main.tf
+++ deploy/terraform/eu-west-2-qa/main.tf
@@ -22,6 +22,24 @@ resource "aws_elastic_beanstalk_application" "default" {
   description = "Andy Val Sample App"
 }
 
+resource "aws_cloudwatch_metric_alarm" "test-alarm" {
+  count = "${length(module.beanstalk-web-app.instances)}"
+  alarm_name                = "andyval-terraform-si-test-alarm${count.index + 1}"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "NetworkIn"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "55000"
+  alarm_description         = "This metric monitors ec2 network in utilization"
+
+  dimensions {
+    InstanceId = "${element(module.beanstalk-web-app.instances, count.index)}"
+  }
+  alarm_actions     = ["arn:aws:sns:eu-west-2:276973094769:andyval-sample-app-alerts"]
+}
+
 module "beanstalk-web-app" {
   source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
   app_name          = "${aws_elastic_beanstalk_application.default.name}"
