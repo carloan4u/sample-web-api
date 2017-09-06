46370b7e5e9ac53c6839199aaf4579aea34e5904
Calum Flanagan <flanagancalum@gmail.com>
2017-09-06 13:53:36 +0100

    Add terraform deploy script

---
+++ deploy/terraform/eu-west-2-prod/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  backend "s3" {
+    bucket = "zuto-terraform-state-files"
+    key    = "services/calflan-sample-app/prod.tfstate"
+    region = "eu-west-2"
+    acl    = "bucket-owner-full-control"
+  }
+}
+
+variable "environment" {
+  default = "prod"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_elastic_beanstalk_application" "default" {
+  name        = "calflan-sample-app"
+  description = "Loan application routing service"
+}
+
+module "beanstalk-web-app" {
+  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
+  app_name          = "${aws_elastic_beanstalk_application.default.name}"
+  instance_type     = "t2.medium"
+  app_environment   = "${var.environment}"
+  asg_min_instances = 2
+  asg_max_instances = 3
+  ec2_key           = "prod-ec2-applications"
+  create_dns_record = true
+  owner_tag         = "CalFlan"
+  healthcheck_url = "/api/status"
+
+  sns_topic = {
+    name = "${aws_elastic_beanstalk_application.default.name}-${var.environment}"
+    endpoint = "arn:aws:sqs:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_elastic_beanstalk_application.default.name}-${var.environment}"
+    protocol = "sqs"
+  }
+}
