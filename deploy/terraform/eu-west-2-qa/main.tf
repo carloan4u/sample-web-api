482496db81fc16928538900bb2f7f93bcb5f047f
Trevor Fawcett <trevor.fawcett@zuto.com>
2017-09-06 16:27:31 +0100

    alerting gubbins

--- deploy/terraform/eu-west-2-qa/main.tf
+++ deploy/terraform/eu-west-2-qa/main.tf
@@ -23,7 +23,7 @@ resource "aws_elastic_beanstalk_application" "default" {
 }
 
 module "beanstalk-web-app" {
-  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
+  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.7"
   app_name          = "${aws_elastic_beanstalk_application.default.name}"
   instance_type     = "t2.small"
   app_environment   = "${var.environment}"
@@ -39,4 +39,22 @@ module "beanstalk-web-app" {
     endpoint = "arn:aws:sqs:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_elastic_beanstalk_application.default.name}-${var.environment}"
     protocol = "sqs"
   }
+}
+
+resource "aws_cloudwatch_metric_alarm" "test-alarm" {
+  count                     = "${length(module.beanstalk-web-app.instances)}"
+  alarm_name                = "terraform-fawcett-test-alarm${count.index + 1}"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "NetworkIn"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "55000"
+  alarm_description         = "This metric monitors ec2 network in utilization"
+
+  dimensions {
+    InstanceId = "${element(module.beanstalk-web-app.instances, count.index)}"
+  }
+  alarm_actions     = ["arn:aws:sns:eu-west-2:276973094769:fawcett-qa"]
 }
