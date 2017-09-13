26064930a8c739c5016fbeba5c65a9d338c70109
David Connor <davidconnor85@gmail.com>
2017-09-13 14:16:59 +0100

    Update prod terraform script.

--- deploy/terraform/eu-west-2-prod/main.tf
+++ deploy/terraform/eu-west-2-prod/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "zuto-terraform-state-files"
-    key    = "services/routing-service-prod.tfstate"
+    key    = "services/dc-workshop-app/qa.tfstate"
     region = "eu-west-2"
     acl    = "bucket-owner-full-control"
   }
@@ -18,20 +18,21 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_elastic_beanstalk_application" "default" {
-  name        = "routing-service"
-  description = "Loan application routing service"
+  name        = "dc-workshop-app"
+  description = "DC Workshop App"
 }
 
 module "beanstalk-web-app" {
   source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
   app_name          = "${aws_elastic_beanstalk_application.default.name}"
-  instance_type     = "t2.medium"
+  instance_type     = "t2.small"
   app_environment   = "${var.environment}"
   asg_min_instances = 2
-  asg_max_instances = 3
-  ec2_key           = "prod-ec2-applications"
+  asg_max_instances = 2
+  ec2_key           = "${var.environment}-ec2-applications"
   create_dns_record = true
-  owner_tag         = "Sales-Ops"
+  owner_tag         = "Acquisition"
+  healthcheck_url   = "/api/status"
 
   sns_topic = {
     name = "${aws_elastic_beanstalk_application.default.name}-${var.environment}"
