a1e2845f18e459938634eab6e42fbb0be790cc2f
Daniel Smith <daniel.smith@zuto.com>
2017-09-13 13:32:23 +0100

    AWS- add qa terraform

---
+++ deploy/terraform/eu-west-2-qa/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  backend "s3" {
+    bucket = "zuto-terraform-state-files"
+    key    = "services/dansmith-sample-app-aws/qa.tfstate"
+    region = "eu-west-2"
+    acl    = "bucket-owner-full-control"
+  }
+}
+
+variable "environment" {
+  default = "qa"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_elastic_beanstalk_application" "default" {
+  name        = "dansmith-sample-app-aws"
+  description = "Sample app for AWS by Dan Smith"
+}
+
+module "beanstalk-web-app" {
+  source            = "git@github.com:carloan4u/terraform-aws-beanstalk-environment-module.git?ref=v1.2.5"
+  app_name          = "${aws_elastic_beanstalk_application.default.name}"
+  instance_type     = "t2.small"
+  app_environment   = "${var.environment}"
+  asg_min_instances = 1
+  asg_max_instances = 2
+  ec2_key           = "qa-ec2-applications"
+  create_dns_record = true
+  owner_tag         = "Sales-Ops"
+  healthcheck_url = "/api/Status"
+
+  sns_topic = {
+    name = "${aws_elastic_beanstalk_application.default.name}-${var.environment}"
+    endpoint = "arn:aws:sqs:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_elastic_beanstalk_application.default.name}-${var.environment}"
+    protocol = "sqs"
+  }
+}
